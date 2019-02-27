98797f5a3dbe3979e33099637e5968614e9e1801
Ilya <tenishev22099@gmail.com>
2019-02-27 16:15:51 +0300

    changes 2.0

--- app/src/main/java/com/example/hp/thenewestproject/HelpCategory.java
+++ app/src/main/java/com/example/hp/thenewestproject/HelpCategory.java
@@ -2,23 +2,13 @@ package com.example.hp.thenewestproject;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import butterknife.BindArray;
 import butterknife.BindBitmap;
-import butterknife.BindDrawable;
-import butterknife.BindInt;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -43,12 +33,6 @@ public class HelpCategory extends AppCompatActivity {
 
     @BindView(R.id.recycler)
     RecyclerView showRecycler;
-
-
-
-
-   // private Bitmap imgs[] = {kids, adults, olds, animals, events};
-
 
 
     @Override
